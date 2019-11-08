ccb598c37ddb0db90030842bf3b953ded5a41730
Wanadat <taywaynday@gmail.com>
2019-11-08 13:51:03 -0800

    Created the window.
    Tawanda C.

--- Project Main/front_end.py
+++ Project Main/front_end.py
@@ -1,1 +1,17 @@
 # initial commit
+from tkinter import *
+
+#Added code to create the window - Tawanda
+
+#created window variable
+Window = Tk()
+
+#Set window title
+Window.title("Please work")
+#Set window dimensions
+Window.maxsize(1000, 1000)
+
+#Start the program
+Window.mainloop()
+
+# End code here 11/8/2019 - Tawanda
