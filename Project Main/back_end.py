5106362b7ee60bedf4e2d70b50819827faa01186
stevenv48 <56889510+stevenv48@users.noreply.github.com>
2019-11-15 13:13:17 -0800

    updated by steven

--- Project Main/back_end.py
+++ Project Main/back_end.py
@@ -2,8 +2,14 @@
 # nothing submitted thus far. Hugo 11_13_2019
 # testing Hugo 11_15_2019
 
-import sqlite3 #   -steven
+import sqlite3 #  -steven
 c = conn.cursor()
+
+#create table
+c.execute('''CREATE TABLE stocks 
+            (date text, trans text, symbol text, qty real, price real)''')
+
+# insert a row of data
 conn.commit()
 
 conn.close()
