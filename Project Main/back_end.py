ab593be0e0e9fafb4f70f9c1fffeb7fce772cb4a
chrismesina14 <005319687@coyote.csusb.edu>
2019-11-15 14:08:34 -0800

    Updated by Christian

--- Project Main/back_end.py
+++ Project Main/back_end.py
@@ -2,15 +2,54 @@
 # nothing submitted thus far. Hugo 11_13_2019
 # testing Hugo 11_15_2019
 
-import sqlite3 #  -steven
+import sqlite3
+
+conn = sqlite3.connect('shoes.db')
+
+
 c = conn.cursor()
 
-#create table
-c.execute('''CREATE TABLE stocks 
-            (date text, trans text, symbol text, qty real, price real)''')
+# Create table
+# c.execute('''CREATE TABLE shoes
+  #            (brand text, model text, release_date text, price text)''')
 
-# insert a row of data
+# Insert a row of data
+c.execute("INSERT INTO shoes VALUES ('Adidas','yeezy boost','2017', 250.0)")
+
+
+def Steven_inserts():
+    brands = input("brand")
+    models = input("model")
+    releases = input("release")
+    prices = input("price")
+
+    c.execute("INSERT INTO shoes (brand,model,release_date,price) VALUES(?,?,?),(brand, model, release, price)")
+    conn.commit()
+    for row in c.execute('SELECT * FROM shoes ORDER BY price'):
+        print(row)
+
+
+# Save (commit) the changes
 conn.commit()
 
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+
+for row in c.execute('SELECT * FROM shoes ORDER BY price'):
+    print(row)
+
+Steven_inserts()
 conn.close()
+
+
+
+
+
+
+
+
+
+conn.close()
+
+
 
