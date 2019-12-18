6a03711aec8e7546ef90878d681705397a630747
chrismesina14 <005319687@coyote.csusb.edu>
2019-12-17 16:09:32 -0800

    Added insert, remove and search functions to be implemented on the table's database system. - Christian and Steven (12/17/19)

--- Project Main/back_end.py
+++ Project Main/back_end.py
@@ -4,7 +4,6 @@
 
 import sqlite3
 
-
 def start():
     conn = sqlite3.connect('shoes.db')
     c = conn.cursor()
@@ -15,7 +14,7 @@ def start():
     conn.close()
 
 
-def Steven_inserts(brands, models, prices):
+def insert(brands, models, prices):
     conn = sqlite3.connect('shoes.db')
     c = conn.cursor()
     c.execute('INSERT INTO shoe Values(?,?,?)', (brands, models, prices))
@@ -26,34 +25,35 @@ def Steven_inserts(brands, models, prices):
 def print_me():
     conn = sqlite3.connect('shoes.db')
     c = conn.cursor()
-    #for row in c.execute('SELECT * FROM shoe ORDER BY price'):
-     #   print(row)
-
-    c.execute('SELECT * FROM shoe ORDER BY price')
-    rows = c.fetchall()
+    for row in c.execute('SELECT * FROM shoe ORDER BY price'):
+        print(row)
     conn.close()
-    return rows
 
 def remove_shoe(Model):
     conn = sqlite3.connect('shoes.db')
     c = conn.cursor()
     c.execute('DELETE FROM shoe WHERE model=?',(Model,))
     conn.commit()
-
     conn.close()
 
+def search_shoe(brand):
+    conn = sqlite3.connect('shoes.db')
+    c = conn.cursor()
+    for row in c.execute('SELECT * FROM shoe WHERE brand=?',(brand,)):
+        print(row)
+    conn.close()
 
 start()
-#print_me()
-# shoe = input("brand")
-# mod = input("model")
-# cash = input("price")
-# Steven_inserts(shoe, mod, cash)
-#print_me()
-#g = input("Enter a shoe to be removed:")
-#remove_shoe(g)
-#print_me()
-# still working on it
-# Hugo 11_15_2019
+print_me()
+shoe = input("brand")
+mod = input("model")
+cash = input("price")
+insert(shoe, mod, cash)
+print_me()
 
-# christian is still working on it...
+r = input("Enter a shoe to be removed: ")
+remove_shoe(r)
+print_me()
+
+s = input("Enter a shoe to be searched: ")
+search_shoe(s)
